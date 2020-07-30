72cdef390ff4744d1841b826ea08ba66a846729e
Dan Murphy <danielmurph8@gmail.com>
2020-07-29 20:06:59 -0400

    Finalized pretty-print of board to console

--- Life.py
+++ Life.py
@@ -1,5 +1,5 @@
 import random
-
+from colorama import Fore, Back
 class GameOfLife:
     def __init__(self, height, width):
         """
@@ -16,14 +16,19 @@ class GameOfLife:
         height = self.height
         width = self.width
         board = [[random.randint(0, 1) for i in range(width)] for j in range(height)]
-        return board
-        def render(board, height, width):
+        def render(board):
             """Helper function to pretty print board."""
-            pass
+            border_width = len(board[0]) + 1
+            border = Back.WHITE + Fore.GREEN + " - "*len(range(border_width)) + Back.RESET            
+            print(border)
+            [print(Fore.GREEN + Back.WHITE + "|" + Back.RESET + "".join([Fore.GREEN  + " " + str(val)+ " " for val in board[i]]) + Back.WHITE + " |" + Back.RESET) for i in range(len(board))]
+            print(border)
+            
+        return render(board)    
 
 
 if __name__ == "__main__":
-    Game = GameOfLife(height = 10, width = 5)
-    print(Game.randomState())
+    Game = GameOfLife(height = 30, width = 30)
+    state = Game.randomState()
     
     
